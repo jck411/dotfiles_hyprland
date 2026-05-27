5eeb5af77816dc63820ed8280fc8c925eb4c9de2
jack <jck411@gmail.com>
2026-05-26 23:10:11 -0400

    feat: add file-roller, disable ScreenCast portal, add DOCUMENTS bookmark
    - Add file-roller to base package list for archive management
    - Disable org.freedesktop.impl.portal.ScreenCast in hyprland portals config
    - Add DOCUMENTS directory to GTK bookmarks
    - Update Thunar accels with new bookmark entry

--- config/Thunar/accels.scm
+++ config/Thunar/accels.scm
@@ -33,6 +33,7 @@
 ; (gtk_accel_path "<Actions>/ThunarWindow/zoom-out-alt" "<Primary>KP_Subtract")
 ; (gtk_accel_path "<Actions>/ThunarWindow/open-file-menu" "F10")
 ; (gtk_accel_path "<Actions>/ThunarWindow/contents" "F1")
+; (gtk_accel_path "<Actions>/ThunarBookmarks/1baa652bf81f2b534a11cc4f1349cfb7" "")
 ; (gtk_accel_path "<Actions>/ThunarWindow/show-highlight" "")
 ; (gtk_accel_path "<Actions>/ThunarStandardView/sort-descending" "")
 ; (gtk_accel_path "<Actions>/ThunarStandardView/sort-by-name" "")
